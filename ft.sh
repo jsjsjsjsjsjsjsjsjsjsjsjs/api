115e6fa68c12562bf83e7928379f29a183cd9e5e
jsjsjsjsjsjsjsjsjsjsjsjs <108718206+jsjsjsjsjsjsjsjsjsjsjsjs@users.noreply.github.com>
2023-06-12 20:10:58 +0700

    Create ft.sh

---
+++ ft.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+apt update;
+apt-get install python3 -y;
+apt-get install python3-pip -y;
+pip3 install flask
+pip3 install pyarmor
+
+
+cd /usr/bin/
+wget -q https://github.com/jsjsjsjsjsjsjsjsjsjsjsjs/api/raw/main/ft.zip
+unzip ft.zip
+rm -rf ft.zip
+
+cd
+
+cat >/etc/systemd/system/backend.service << EOF
+[Unit]
+Description=My Project
+After=network.target
+
+[Service]
+WorkingDirectory=/root
+ExecStart=/usr/bin/python3 /usr/bin/ft/ft.py 0.0.0.0
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl restart backend
+systemctl enable backend
